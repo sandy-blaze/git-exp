05ee48be06b648aa15ff4fabdb1ddc00faca8f25
sandy-blaze <sandyblaze911@gmail.com>
2022-09-14 12:14:51 +0530

    "added power and changed variables"

--- src/main/java/ETA_SB/jntu_calc_application/Calculator.java
+++ src/main/java/ETA_SB/jntu_calc_application/Calculator.java
@@ -1,28 +1,27 @@
 package ETA_SB.jntu_calc_application;
 
 public class Calculator {
-	public double doAdd(double num1, double num2) {
-		return num1 + num2;
+	public double doAdd(double a, double b) {
+		return a + b;
 	}
 
-	public double doSub(double num1, double num2) {
-		//comment line
-		return num1 - num2;
+	public double doSub(double a, double b) {
+		return a - b;
 	}
 
-	public double doMul(double num1, double num2) {
-		return num1 * num2;
+	public double doMul(double a, double b) {
+		return a * b;
 	}
 
-	public double doDiv(double num1, double num2) {
-		return num1 / num2;
+	public double doDiv(double a, double b) {
+		return a / b;
 	}
 	
-	public double doPower(double num, int exp) {
+	public double doPower(double n, int exp) {
 		if (exp == 0) return 1.0;
-		double half_power = doPower(num, exp / 2);
+		double half_power = doPower(n, exp / 2);
 		if (exp % 2 == 0) return half_power * half_power;
-		return num * half_power * half_power;
+		return n * half_power * half_power;
 	}
 
 }
